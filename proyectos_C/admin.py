4f0fa24ad2f85bdf77d31da8296d1c1c7227e3ce
Jose Luis Muñoz <42367982+ioseluiz@users.noreply.github.com>
2025-07-30 08:34:50 -0500

    Import Export Proyecto_CC_Estimado_Conceptual

--- proyectos_C/admin.py
+++ proyectos_C/admin.py
@@ -1,12 +1,19 @@
 from django.contrib import admin
 from import_export.admin import ImportExportModelAdmin
 
-from .resources import Proyecto_CC_Resource
+from .resources import Proyecto_CC_Resource, Proyecto_CC_Estimado_Conceptal_Resource
 
-from .models import Proyecto_CC
+from .models import Proyecto_CC, Proyecto_CC_Estimado_Conceptual
 
 @admin.register(Proyecto_CC)
 class Proyecto_CCAdmin(ImportExportModelAdmin):
     resource_class = Proyecto_CC_Resource
     list_display = ('codigo','title','fecha_entrada','fecha_envio_FIO','seccion','coordinador','estado','asignacion_presup_final','precio_acp')
     search_fields =('codigo','title')
+
+@admin.register(Proyecto_CC_Estimado_Conceptual)
+class Proyecto_CC_Estimado_Conceptual_Admin(ImportExportModelAdmin):
+    resource_class = Proyecto_CC_Estimado_Conceptal_Resource
+    list_display = ('proyecto_cc', 'estimado_conceptual')
+    search_fields = ('proyecto_cc__codigo', 'estimado_conceptual__codigo')
+    autocomplete_fields = ['proyecto_cc', 'estimado_conceptual']
--- proyectos_C/resources.py
+++ proyectos_C/resources.py
@@ -1,7 +1,8 @@
 from import_export import resources, fields
 from import_export.widgets import ForeignKeyWidget
 
-from .models import Proyecto_CC
+from .models import Proyecto_CC, Proyecto_CC_Estimado_Conceptual
+from proyectos_E.models import Proyecto_E
 
 class Proyecto_CC_Resource(resources.ModelResource):
     class Meta:
@@ -26,3 +27,26 @@ class Proyecto_CC_Resource(resources.ModelResource):
         # --- Important settings for import behaviour ---
         skip_unchanged = True # If True, rows that have not changed won't be updated
         report_skipped = True # If True, reports skipped rows and reasons
+
+class Proyecto_CC_Estimado_Conceptal_Resource(resources.ModelResource):
+
+    proyecto_cc = fields.Field(
+        column_name='proyecto_cc',
+        attribute='proyecto_cc',
+        widget=ForeignKeyWidget(Proyecto_CC, 'codigo')
+    )
+
+    estimado_conceptual = fields.Field(
+        column_name='estimado_conceptual',
+        attribute='estimado_conceptual',
+        widget=ForeignKeyWidget(Proyecto_E, 'codigo')
+    )
+
+    class Meta:
+        model = Proyecto_CC_Estimado_Conceptual
+        import_id_fields = ['proyecto_cc', 'estimado_conceptual']
+
+        fields = ('proyecto_cc', 'estimado_conceptual')
+
+        skip_unchanged = True
+        report_skipped = True
