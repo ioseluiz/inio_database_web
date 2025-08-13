f7169d2b033e6995fb900bf27fcf4f19a1f47cd2
Jose Luis Muñoz <42367982+ioseluiz@users.noreply.github.com>
2025-08-13 13:16:19 -0500

    fix production file

--- config/settings/base.py
+++ config/settings/base.py
@@ -9,6 +9,7 @@ load_dotenv()
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 DEBUG = os.getenv('DEBUG', 'False') == 'True'
+print(DEBUG)
 
 
 # Quick-start development settings - unsuitable for production
--- config/settings/production.py
+++ config/settings/production.py
@@ -1,16 +1,17 @@
 from .base import *
 import os
 
+
 if not DEBUG:
     allowed_hosts_string = os.getenv('ALLOWED_HOSTS')
     
     # Convierte la cadena en una lista, separando por las comas.
     # Si la variable no está definida, devuelve una lista vacía para evitar errores.
     ALLOWED_HOSTS = allowed_hosts_string.split(',') if allowed_hosts_string else []
-
-    SECURE_SSL_REDIRECT = True
-    SESSION_COOKIE_SECURE = True
-    CSRF_COOKIE_SECURE = True
-    SECURE_HSTS_SECONDS = 31536000 # 1 año
-    SECURE_HSTS_INCLUDE_SUBDOMAINS = True
-    SECURE_HSTS_PRELOAD = True
+    # SECURE_SSL_REDIRECT = True
+    # SESSION_COOKIE_SECURE = True
+    # CSRF_COOKIE_SECURE = True
+    # SECURE_HSTS_SECONDS = 31536000 # 1 año
+    # SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+    # SECURE_HSTS_PRELOAD = True
+    # print(ALLOWED_HOSTS)
