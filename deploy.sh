f274abf676ae639b820f33e49563ae326a75ba78
Jose Luis Muñoz <42367982+ioseluiz@users.noreply.github.com>
2025-11-06 13:42:56 -0500

    file deploy.sh added

---
+++ deploy.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+# activate the virtual environment
+source ~/venv/bin/activate
+
+# cd into the project code
+cd /var/www/inio_database_web
+
+# pull
+git pull
+
+# Install the app dependencies
+pip install -r requirements.txt
+
+# Run the collect static command
+python manage.py collectstatic --no-imput
+
+# put all other command that required for your specific app
+
+# deactivate
+
+# reload nginx
+sudo systemctl reload nginx
+
+# restart the gunicorn
+sudo systemctl restart gunicorn
